0a19c744e045c5f6af3cf4785c79e29b7f39d5bd
randypalusz <ibanezguy998@gmail.com>
2022-03-04 02:16:53 -0500

    add is_string_permutable_to_palindrome

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -3,7 +3,19 @@ from test_framework import generic_test
 
 def can_form_palindrome(s: str) -> bool:
     # TODO - you fill in here.
-    return True
+    hist = {}
+    # iterate over string
+    # add into map when char first seen
+    # otherwise if already seen, increment num by 1
+    for char in s:
+        if char not in hist:
+            hist[char] = 1
+        else:
+            hist[char] = hist[char] + 1
+    # get list of whether each char is seen an odd number of times
+    num_odd = [val % 2 != 0 for val in hist.values()]
+    # return True if the odd count <= 1
+    return num_odd.count(True) <= 1
 
 
 if __name__ == '__main__':

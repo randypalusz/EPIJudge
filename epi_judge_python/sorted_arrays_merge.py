09449d3d2362abcc8c3dae3344a7c79cbb2f0437
randypalusz <ibanezguy998@gmail.com>
2022-03-04 01:27:14 -0500

    added sol'n for sorted_arrays_merge.py

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -5,7 +5,22 @@ from test_framework import generic_test
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    ret = []
+    # array keeping track of the given indices
+    min_heap = [0] * len(sorted_arrays)
+    for idx, val in enumerate(sorted_arrays):
+        min_heap[idx] = val.pop(0)
+    while not all(element == float("inf") for element in min_heap):
+        # get min value in min_heap
+        min_val = min(min_heap)
+        idx = min_heap.index(min_val)
+        ret.append(min_heap[idx])
+        if sorted_arrays[idx]:
+            min_heap[idx] = sorted_arrays[idx].pop(0)
+        else:
+            min_heap[idx] = float("inf")
+
+    return ret
 
 
 if __name__ == '__main__':

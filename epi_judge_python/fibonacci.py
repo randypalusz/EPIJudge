79eb4c5413321ed5d04742813edbeed3d762cc50
randypalusz <ibanezguy998@gmail.com>
2022-03-08 20:09:41 -0500

    add fibonacci.py

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def fibonacci(n: int) -> int:
     # TODO - you fill in here.
-    return -1
+    vals = []
+    for i in range(n+1):
+        if i == 0:
+            vals.append(0)
+        elif i == 1:
+            vals.append(1)
+        else:
+            vals.append(vals[i-1] + vals[i-2])
+    return vals[n]
 
 
 if __name__ == '__main__':

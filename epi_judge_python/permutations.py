cccf735ec0790e5a21392896617d9797adf0f6b8
randypalusz <ibanezguy998@gmail.com>
2022-03-09 19:38:41 -0500

    add permutations.py

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -5,7 +5,17 @@ from test_framework import generic_test, test_utils
 
 def permutations(A: List[int]) -> List[List[int]]:
     # TODO - you fill in here.
-    return []
+    def helper(in_list: List[int], current_perm: List[int], all_perms: List[List[int]]):
+        if not in_list:
+            all_perms.append(current_perm)
+            return
+        for idx, val in enumerate(in_list):
+            helper(in_list[:idx] + in_list[idx+1:],
+                   current_perm + [val], all_perms)
+    all_permutations = []
+    helper(A, [], all_permutations)
+
+    return all_permutations
 
 
 if __name__ == '__main__':
